fbd5e1a18abaee92da57ff7618e33b4f8bf6f1c9
Pablo <pablofuente14@opendeusto.es>
2022-05-24 02:41:16 +0200

    busqueda completa

--- Aplicaciones/Gestion/views.py
+++ Aplicaciones/Gestion/views.py
@@ -91,20 +91,27 @@ def productos(request):
     productos = None
     texto_busqueda = request.POST.get("texto_busqueda", "")
     if texto_busqueda:
-        productos =  Producto.objects.filter(
-            Q(nombre__icontains=texto_busqueda) | 
-            Q(descripcion__icontains=texto_busqueda)
+        productos = Producto.objects.filter(
+            Q(nombre__icontains=texto_busqueda) |
+            Q(descripcion__icontains=texto_busqueda) |
+            Q(referencia__icontains=texto_busqueda) |
+            Q(precio__icontains=texto_busqueda) |
+            Q(categoria__nombre__icontains=texto_busqueda)|
+            Q(componentes__nombre__icontains = texto_busqueda) 
         )
     else:
         productos = Producto.objects.all()
-        
+
     cantidad_por_pagina = 5
-    paginator = Paginator(productos, cantidad_por_pagina) # E3 - Paginador de productos, con 5 de maximo
-    pagina = request.GET.get("page", 1) # Obtenemos el número de página que queremos
-    productos = paginator.get_page(pagina) # Obtenemos los productos de esa pagina
-    pagina_actual = int(pagina) # Página actual
-    paginas = range(1, productos.paginator.num_pages + 1) # Total de páginas
-    
+    # E3 - Paginador de productos, con 5 de maximo
+    paginator = Paginator(productos, cantidad_por_pagina)
+    # Obtenemos el número de página que queremos
+    pagina = request.GET.get("page", 1)
+    # Obtenemos los productos de esa pagina
+    productos = paginator.get_page(pagina)
+    pagina_actual = int(pagina)  # Página actual
+    paginas = range(1, productos.paginator.num_pages + 1)  # Total de páginas
+
     categorias = Categoria.objects.all()
     componentes = Componente.objects.all()
     return render(request, 'productos/gestion_productos.html', {
@@ -139,35 +146,36 @@ def ediccionProducto(request, referencia):
     producto = Producto.objects.get(referencia=referencia)
     categorias = Categoria.objects.all()
     componentes = Componente.objects.all()
-    
+
     componentes_calculados = []
-    
+
     for comp in componentes:
         componente_es_usado_en_producto = False
         for pcomp in producto.componentes.all():
             if pcomp.referencia == comp.referencia:
                 componente_es_usado_en_producto = True
-        
-        tupla = (comp, componente_es_usado_en_producto) # Esta tupla tiene en el indice 0 el componente y en el indice 1 un booleano que indica si el compoente pertence al producto
-        componentes_calculados.append(tupla)    
-    
+
+        # Esta tupla tiene en el indice 0 el componente y en el indice 1 un booleano que indica si el compoente pertence al producto
+        tupla = (comp, componente_es_usado_en_producto)
+        componentes_calculados.append(tupla)
+
     print(componentes_calculados)
-    return render(request,"productos/ediccion_producto.html", {'producto':producto,'categorias':categorias,'componentes_formato_tupla':componentes_calculados })
+    return render(request, "productos/ediccion_producto.html", {'producto': producto, 'categorias': categorias, 'componentes_formato_tupla': componentes_calculados})
 
 
 def editarProducto(request):
     referencia = request.POST["referencia"]
     nombre = request.POST["nombre"]
-    precio = float(request.POST["precio"].replace(",","."))
+    precio = float(request.POST["precio"].replace(",", "."))
     descripcion = request.POST["descripcion"]
     id_categoria = request.POST["categorias"]
     categoria = Categoria.objects.get(id=id_categoria)
     referencias_componentes = request.POST.getlist("componentes")
-    producto= Producto.objects.get(referencia=referencia)
-    producto.nombre=nombre
-    producto.precio=precio
-    producto.descripcion=descripcion
-    producto.categoria=categoria
+    producto = Producto.objects.get(referencia=referencia)
+    producto.nombre = nombre
+    producto.precio = precio
+    producto.descripcion = descripcion
+    producto.categoria = categoria
     for componente in producto.componentes.all():
         producto.componentes.remove(componente)
     for referencia in referencias_componentes:
@@ -183,6 +191,7 @@ def borrarProducto(request, referencia):
     return redirect("/productos")
 
     # CLIENTES
+
 
 def clientes(request):
     clientes = Cliente.objects.all()
@@ -230,43 +239,48 @@ def borrarCliente(request, cif):
     cliente.delete()
     return redirect("/clientes")
 
+    # PEDIDOS
 
-    #PEDIDOS
-    
+
 def pedidos(request):
     pedidos = Pedido.objects.all()
     return render(request, 'pedidos/gestion_pedidos.html', {'pedidos': pedidos})
 
 
 def nuevoPedido(request):
-    clientes= Cliente.objects.all()
-    productos= Producto.objects.all()
-    return render(request, 'pedidos/nuevo_pedido.html',{'clientes': clientes , 'productos':productos} )
+    clientes = Cliente.objects.all()
+    productos = Producto.objects.all()
+    return render(request, 'pedidos/nuevo_pedido.html', {'clientes': clientes, 'productos': productos})
+
 
 def registrarPedido(request):
-    referencia= request.POST["referencia"]
+    referencia = request.POST["referencia"]
     if not Pedido.objects.filter(referencia=referencia).exists():
-        fecha= request.POST["fecha"]
-        cif_cliente= request.POST["clientes"]
-        cliente= Cliente.objects.get(cif=cif_cliente)
-        precio_total= 0
-        pedido= Pedido.objects.create(referencia=referencia, fecha=fecha, cliente=cliente, precio=precio_total)
-        referencia_producto= request.POST["productos"]
-        producto= Producto.objects.get(referencia= referencia_producto)
-        cantidad= int(request.POST["cantidad"])
-        linea_pedido= LineaPedido.objects.create(producto=producto, cantidad=cantidad, pedido=pedido)
-        precio_total=round(producto.precio * cantidad, 2)
-        pedido.precio=precio_total
+        fecha = request.POST["fecha"]
+        cif_cliente = request.POST["clientes"]
+        cliente = Cliente.objects.get(cif=cif_cliente)
+        precio_total = 0
+        pedido = Pedido.objects.create(
+            referencia=referencia, fecha=fecha, cliente=cliente, precio=precio_total)
+        referencia_producto = request.POST["productos"]
+        producto = Producto.objects.get(referencia=referencia_producto)
+        cantidad = int(request.POST["cantidad"])
+        linea_pedido = LineaPedido.objects.create(
+            producto=producto, cantidad=cantidad, pedido=pedido)
+        precio_total = round(producto.precio * cantidad, 2)
+        pedido.precio = precio_total
         pedido.save()
     return redirect("/pedidos")
+
 
 def ediccionPedido(request, referencia):
     pedido = Pedido.objects.get(referencia=referencia)
     clientes = Cliente.objects.all()
     productos = Producto.objects.all()
-    linea_pedido= LineaPedido.objects.get(pedido=pedido)
+    linea_pedido = LineaPedido.objects.get(pedido=pedido)
     print(linea_pedido)
-    return render(request, "pedidos/ediccion_pedido.html", {'pedido': pedido, 'clientes':clientes, 'productos':productos, 'linea_pedido':linea_pedido})
+    return render(request, "pedidos/ediccion_pedido.html", {'pedido': pedido, 'clientes': clientes, 'productos': productos, 'linea_pedido': linea_pedido})
+
 
 def editarPedido(request):
     id_pedido = request.POST["referencia"]
@@ -275,25 +289,18 @@ def editarPedido(request):
     producto_referencia = request.POST["productos"]
     cantidad = request.POST["cantidad"]
 
-    pedido = Pedido.objects.get(referencia = id_pedido)
+    pedido = Pedido.objects.get(referencia=id_pedido)
     pedido.fecha = fecha
     pedido.cliente = Cliente.objects.get(cif=cliente_cif)
-    
+
     producto = Producto.objects.get(referencia=producto_referencia)
-    
+
     # de momento asumimos que habra una sola linea de pedido y solo una
     lp = pedido.lineapedido_set.all()[0]
-    
+
     lp.cantidad = cantidad
     lp.producto = producto
-    
+
     lp.save()
     pedido.save()
     return redirect("/pedidos")
-    
-    
-  
-    
-    
-    
-  
